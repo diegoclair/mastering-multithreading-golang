68c8f1e303cabf21e48eed7d4b5431233db47da5
Diego Clair <diego93rodrigues@gmail.com>
2022-02-18 21:47:21 -0300

    feat: add thread pool into the example

--- threadpool/main.go
+++ threadpool/main.go
@@ -6,8 +6,10 @@ import (
 	"math"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -21,23 +23,38 @@ import (
 */
 
 /*
-	avarage time before thread pools ----> The processing took 7.6891032s
-	avarage time before thread pools ---->
+	avarage time before thread pools ----> The processing took 6.2891032s
+	avarage time after  thread pools ----> The processing took 4.8891032s
 */
+
+var (
+	numberOfThreads int = runtime.NumCPU()
+	waitGroup           = sync.WaitGroup{}
+)
+
 type Point2D struct {
 	x int
 	y int
 }
 
 func main() {
+
 	absPath, _ := filepath.Abs("./")
 	dat, _ := ioutil.ReadFile(filepath.Join(absPath, "polygons.txt"))
 	text := string(dat)
 
+	inputChannel := make(chan string, 1000) //we can put 1000 values in the channel, like a queue
+	for i := 0; i < numberOfThreads; i++ {
+		go findArea(inputChannel)
+	}
+	waitGroup.Add(numberOfThreads)
+
 	start := time.Now()
 	for _, line := range strings.Split(text, "\n") {
-		findArea(line)
+		inputChannel <- line
 	}
+	close(inputChannel)
+	waitGroup.Wait() //when we close the channel, the other threads my have not finished to process the queue of inputchannel.. so we can use the waitGroup to synchronize
 
 	fmt.Println(`The processing took`, time.Since(start))
 
@@ -47,22 +64,25 @@ var (
 	r = regexp.MustCompile(`\((\d*),(\d*)\)`)
 )
 
-func findArea(pointsStr string) {
-	var points []Point2D
-	matches := r.FindAllStringSubmatch(pointsStr, -1)
+func findArea(inputChannel chan string) {
+	for pointsStr := range inputChannel {
+		var points []Point2D
+		matches := r.FindAllStringSubmatch(pointsStr, -1)
 
-	for _, p := range matches {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{x: x, y: y})
+		for _, p := range matches {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{x: x, y: y})
+		}
+
+		area := 0.0
+		pointsLenght := len(points)
+		for i := 0; i < pointsLenght; i++ {
+			a, b := points[i], points[(i+1)%pointsLenght]
+			area += float64(a.x*b.y) - float64(a.y*b.x)
+		}
+
+		fmt.Println(math.Abs(area) / 2.0)
 	}
-
-	area := 0.0
-	pointsLenght := len(points)
-	for i := 0; i < pointsLenght; i++ {
-		a, b := points[i], points[(i+1)%pointsLenght]
-		area += float64(a.x*b.y) - float64(a.y*b.x)
-	}
-
-	fmt.Println(math.Abs(area) / 2.0)
+	waitGroup.Done()
 }
